677dfd4e6d7d717122edeb3e8460dd1704dff60b
russkovmatvey <russkov_matvey@mail.ru>
2018-12-08 23:30:23 +0300

    renamed something

--- src/main/java/matvey/russkov/parser/Parser.java
+++ src/main/java/matvey/russkov/parser/Parser.java
@@ -9,17 +9,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Parser {
-    private ArrayList story = new ArrayList();//30 url считывает со страницы, но невсегда.
+    private ArrayList arrayOfStories = new ArrayList();//30 url считывает со страницы, но невсегда.
 
-    public void getStory() throws IOException {
+    public void getStories() throws IOException {
         final int countPage = 300;//количество страниц
         for (int i = 0; i < countPage; i++) {
             Document doc = Jsoup.connect("https://www.fmylife.com/tops?page=" + Integer.toString(i)).get();
             Elements blocks = doc.getElementsByAttributeValue("class", "block hidden-xs");
             for (Element block : blocks) {
                 Element body = block.child(0);
-                Elements text = body.getElementsByAttributeValue("href", body.attr("href"));
-                story.add(text.text());
+                Elements story = body.getElementsByAttributeValue("href", body.attr("href"));
+                arrayOfStories.add(story.text());
             }
         }
     }
