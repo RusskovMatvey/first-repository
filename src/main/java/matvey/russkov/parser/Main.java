0c3e9fa1f5b65a00744aab305f8b37a12142e855
russkovmatvey <russkov_matvey@mail.ru>
2018-12-08 21:10:28 +0300

    add parser 2.0

--- src/main/java/matvey/russkov/parser/Main.java
+++ src/main/java/matvey/russkov/parser/Main.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Parser.findUrlWithText();
-        Parser.readText();
+        Parser parser = new Parser();
+
+        parser.findUrlWithText();
+        parser.readText();
     }
 }
--- src/main/java/matvey/russkov/parser/Parser.java
+++ src/main/java/matvey/russkov/parser/Parser.java
@@ -6,30 +6,30 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Parser {
-    private static Vector url = new Vector();//22 url считывает со страницы
-    private static Vector text = new Vector();
+    private ArrayList url = new ArrayList();//30 url считывает со страницы
+    private ArrayList text = new ArrayList();
 
-    public static void findUrlWithText() throws IOException {
-        Document doc = Jsoup.connect("https://www.fmylife.com").get();
-        Elements pElements = doc.getElementsByAttributeValue("class", "block hidden-xs");
+    public void findUrlWithText() throws IOException {
+        for (int i = 0; i <= 2; i++) {
+            Document doc = Jsoup.connect("https://www.fmylife.com/tops?page=" + Integer.toString(i)).get();
+            Elements pElements = doc.getElementsByAttributeValue("class", "block hidden-xs");
 
-        int i = 0;
-        for (Element pElement : pElements) {
-            Element aElement = pElement.child(0);
-            url.add(aElement.attr("href"));
-            i++;
+            for (Element pElement : pElements) {
+                Element aElement = pElement.child(0);
+                url.add(aElement.attr("href"));
+            }
+            System.out.println(url.size());
         }
     }
 
-    public static void readText() throws IOException {
-        for(int i = 0; i < url.size(); i++) {
+    public void readText() throws IOException {
+        for (int i = 0; i < url.size(); i++) {
             Document doc = Jsoup.connect("https://www.fmylife.com" + url.get(i)).get();
             Elements pElements = doc.getElementsByAttributeValue("class", "block hidden-xs");
             text.add(pElements.text());
-            System.out.println(text.get(i));
         }
     }
 }
