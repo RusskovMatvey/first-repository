c75de42240c46af3c1501b2708e6b019075ff277
russkovmatvey <russkov_matvey@mail.ru>
2018-12-08 22:42:20 +0300

    optimized the parser

--- src/main/java/matvey/russkov/parser/Main.java
+++ src/main/java/matvey/russkov/parser/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Parser parser = new Parser();
 
-        parser.findUrlWithText();
-        parser.readText();
+        parser.getStory();
     }
 }
--- src/main/java/matvey/russkov/parser/Parser.java
+++ src/main/java/matvey/russkov/parser/Parser.java
@@ -9,27 +9,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Parser {
-    private ArrayList url = new ArrayList();//30 url считывает со страницы
-    private ArrayList text = new ArrayList();
+    private ArrayList story = new ArrayList();//30 url считывает со страницы, но невсегда.
 
-    public void findUrlWithText() throws IOException {
-        for (int i = 0; i <= 2; i++) {
+    public void getStory() throws IOException {
+        int countPage = 300;//количество страниц
+        for (int i = 0; i < countPage; i++) {
             Document doc = Jsoup.connect("https://www.fmylife.com/tops?page=" + Integer.toString(i)).get();
-            Elements pElements = doc.getElementsByAttributeValue("class", "block hidden-xs");
-
-            for (Element pElement : pElements) {
-                Element aElement = pElement.child(0);
-                url.add(aElement.attr("href"));
+            Elements blocks = doc.getElementsByAttributeValue("class", "block hidden-xs");
+            for (Element block : blocks) {
+                Element body = block.child(0);
+                Elements text = body.getElementsByAttributeValue("href", body.attr("href"));
+                story.add(text.text());
             }
-            System.out.println(url.size());
-        }
-    }
-
-    public void readText() throws IOException {
-        for (int i = 0; i < url.size(); i++) {
-            Document doc = Jsoup.connect("https://www.fmylife.com" + url.get(i)).get();
-            Elements pElements = doc.getElementsByAttributeValue("class", "block hidden-xs");
-            text.add(pElements.text());
         }
     }
 }
