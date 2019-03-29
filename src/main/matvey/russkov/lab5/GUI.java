4136c9341e09803b8afc12c52418be0cc0e14acc
RusskovMatvey <russkov_matvey@mail.ru>
2019-03-29 11:50:52 +0300

    use GridBagLayout for lab 5

--- src/main/matvey/russkov/lab5/GUI.java
+++ src/main/matvey/russkov/lab5/GUI.java
@@ -4,21 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowStateListener;
 
 public class GUI extends JFrame {
 
-    private JButton button1 = new JButton("Task #1");
-    private JButton button2 = new JButton("Task #2");
-    private JButton button3 = new JButton("Task #3");
-    private JButton button4 = new JButton("Task #4");
+    private JButton button1 = new JButton("Программа №1");
+    private JButton button2 = new JButton("Программа №2");
+    private JButton button3 = new JButton("Программа №3");
+    private JButton button4 = new JButton("Программа №4");
     private JPanel main_panel = new JPanel();
-    private JPanel panel = new JPanel();
     private JTextField input1 = new JTextField();
-    private JTextField input2 = new JTextField();
-    private JTextField input3 = new JTextField();
-    private JLabel label = new JLabel();
+    private JLabel labelOutput = new JLabel("Вывод:");
+    private JLabel labelArguments = new JLabel("Аргументы:");
     static public JTextArea textArea = new JTextArea(10, 6);
     private JScrollPane scroll = new JScrollPane(textArea);
 
@@ -27,10 +23,7 @@ public class GUI extends JFrame {
             @Override
             public void mouseClicked(MouseEvent e) {
                 textArea.setText("");
-                String[] args = new String[2];
-                args[0] = input1.getText();
-                args[1] = input2.getText();
-                main.matvey.russkov.lab1.Main.main(args);
+                main.matvey.russkov.lab1.Main.main(input1.getText().split(" "));
             }
 
             @Override
@@ -53,11 +46,7 @@ public class GUI extends JFrame {
             @Override
             public void mouseClicked(MouseEvent e) {
                 textArea.setText("");
-                String[] args = new String[3];
-                args[0] = input1.getText();
-                args[1] = input2.getText();
-                args[2] = input3.getText();
-                main.matvey.russkov.lab2.Main.main(args);
+                main.matvey.russkov.lab2.Main.main(input1.getText().split(" "));
             }
 
             @Override
@@ -84,10 +73,8 @@ public class GUI extends JFrame {
             @Override
             public void mouseClicked(MouseEvent e) {
                 textArea.setText("");
-                String[] args = new String[1];
-                args[0] = input1.getText();
                 try {
-                    main.matvey.russkov.lab3ex.Main.main(args);
+                    main.matvey.russkov.lab3ex.Main.main(input1.getText().split(" "));
                 } catch (Exception ex) {
                     textArea.setText("Input Error!");
                 }
@@ -117,11 +104,8 @@ public class GUI extends JFrame {
             @Override
             public void mouseClicked(MouseEvent e) {
                 textArea.setText("");
-                String[] args = new String[2];
-                args[0] = input1.getText();
-                args[1] = input2.getText();
                 try {
-                    main.matvey.russkov.lab4.Main.main(args);
+                    main.matvey.russkov.lab4.Main.main(input1.getText().split(" "));
                 } catch (InterruptedException e1) {
                     e1.printStackTrace();
                 }
@@ -150,64 +134,60 @@ public class GUI extends JFrame {
     }
 
     GUI() {
-        super.setSize(640, 480);
-        super.setTitle("Task #5");
+        super.setSize(1024, 640);
+        super.setTitle("Программа №5");
         main_panel.setVisible(true);
         super.setExtendedState(MAXIMIZED_BOTH);
         super.add(main_panel);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Я добавил
-        /*addWindowStateListener(new WindowStateListener() {
-            @Override
-            public void windowStateChanged(WindowEvent windowEvent) {
-                if (windowEvent.getNewState() == GUI.MAXIMIZED_BOTH) {
-                    dispose();
-                    setUndecorated(true);
-                    showFrame();
-                } else {
-                    dispose();
-                    setUndecorated(false);
-                    showFrame();
-                }
-            }
-            private void showFrame() {
-                pack();
-                setVisible(true);
-            }
-        });*/
-        //ЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯ
 
         super.setLocationRelativeTo(null);
         main_panel.setLayout(new GridBagLayout());
-        panel.setLayout(new GridBagLayout());
         scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         textArea.setEditable(false);
+        labelOutput.setFont(new Font("Arial", 20, 20));
+        labelArguments.setFont(new Font("Arial", 20, 20));
+        button1.setFont(new Font("Arial", 20, 20));
+        button2.setFont(new Font("Arial", 20, 20));
+        button3.setFont(new Font("Arial", 20, 20));
+        button4.setFont(new Font("Arial", 20, 20));
 
         addListeners();
-        main_panel.add(button1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
-                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 15, 0), 40, 20));
+        final int xButton = 0;
 
-        main_panel.add(button2, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
-                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 15, 0), 40, 20));
+        /*main_panel.add(BorderLayout.CENTER, labelArguments);
+        main_panel.add(BorderLayout.NORTH, button1);
+        main_panel.add(BorderLayout.CENTER, button2);
+        main_panel.add(BorderLayout.CENTER, button3);
+        main_panel.add(BorderLayout.CENTER, button4);
+        main_panel.add(BorderLayout.CENTER, input1);
+        main_panel.add(BorderLayout.CENTER, labelOutput);
+        main_panel.add(BorderLayout.CENTER, scroll);*/
+        main_panel.add(button1, new GridBagConstraints(xButton, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 15, 0), 40, 30));
 
-        main_panel.add(button3, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
-                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 15, 0), 40, 20));
+        main_panel.add(button2, new GridBagConstraints(xButton, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 15, 0), 40, 30));
 
-        main_panel.add(button4, new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
-                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 15, 0), 40, 20));
+        main_panel.add(button3, new GridBagConstraints(xButton, 2, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 15, 0), 40, 30));
 
-        main_panel.add(input1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
-                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 30, 20));
-        main_panel.add(input2, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
-                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 30, 20));
-        main_panel.add(input3, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
-                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 30, 20));
+        main_panel.add(button4, new GridBagConstraints(xButton, 3, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 15, 0), 40, 30));
 
-        main_panel.add(scroll, new GridBagConstraints(0, 2, 4, 100, 0.0, 0.0, GridBagConstraints.NORTH,
-                GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 40, 20));
+        main_panel.add(labelArguments, new GridBagConstraints(1, 0, 4, 100, 0.0, 0.0, GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL, new Insets(0, 20, 0, 0), 40, 20));
+
+        main_panel.add(input1, new GridBagConstraints(1, 1, 4, 100, 0.0, 0.0, GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL, new Insets(0, 20, 0, 0), 40, 20));
+
+        main_panel.add(labelOutput, new GridBagConstraints(1, 2, 4, 100, 0.0, 0.0, GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL, new Insets(0, 20, 0, 0), 40, 20));
+
+        main_panel.add(scroll, new GridBagConstraints(1, 3, 4, 100, 0.0, 0.0, GridBagConstraints.NORTH,
+                GridBagConstraints.VERTICAL, new Insets(0, 20, 0, 0), 300, 40));
+
         super.setVisible(true);
-        super.setResizable(false);
-        main_panel.setVisible(true);
-
+        super.setResizable(true);
     }
 }
